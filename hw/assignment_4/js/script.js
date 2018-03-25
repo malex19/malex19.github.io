ef09718c5e361d9ef2df2e3ad56f875e99705f5f
Alex Arellano <maa418@miami.edu>
2018-03-25 18:31:00 -0400

    00

--- hw/assignment_4/js/script.js
+++ hw/assignment_4/js/script.js
@@ -1,4 +1,24 @@
 $(function(){
-  alert("Hello");
+  //Hide stuff
 
+  $('#getStarted').hide();
+
+  //alert("Hello");
+  $('#title').on('mouseover',function(){
+    $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+    $('#getStarted').show();
+  });
+  $('#title').on('mouseleave',function(){
+    $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+  });
+
+$('#next').on('click',function(){
+  var currentItem = $('li.active');
+  var nextItem = $('li.active').next();
+
+  currentItem.toggleClass('active');
+
+  nextItem.toggleClass('active');
+
+});
 });
