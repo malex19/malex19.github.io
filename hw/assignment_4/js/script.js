b62ef4a12a0f8c0db79f44c2473c7b91690ca211
Alex Arellano <maa418@miami.edu>
2018-03-28 12:26:24 -0400

    00

--- hw/assignment_4/js/script.js
+++ hw/assignment_4/js/script.js
@@ -1,7 +1,7 @@
 $(function(){
   //Hide stuff
 
-  $('#getStarted').hide();
+  $('#getStarted, #finish, #previous, .hide').hide();
 
   //alert("Hello");
   $('#title').on('mouseover',function(){
@@ -10,7 +10,16 @@ $(function(){
   });
   $('#title').on('mouseleave',function(){
     $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+
   });
+$('#getStarted').on('click', function(){
+  $('.hide').show();
+  $('#getStarted').hide();
+});
+$('#finish').on('click', function(){
+  $('.hide, #next, #previous, #finish').hide();
+
+});
 
 $('#next').on('click',function(){
   var currentItem = $('li.active');
@@ -20,5 +29,41 @@ $('#next').on('click',function(){
 
   nextItem.toggleClass('active');
 
-});
+  if($('li').last().hasClass('active')) {
+    $('#next').hide();
+    $('#finish').show();
+  } else {
+    $('#next').show();
+
+  }
+  if($('li').first().hasClass('active')) {
+    $('#previous').hide();
+  } else {
+    $('#previous').show();
+  }
+  });
+
+  $('#previous').on('click',function(){
+    var currentItem = $('li.active');
+    var previousItem = $('li.active').prev();
+
+    currentItem.toggleClass('active');
+
+    previousItem.toggleClass('active');
+
+    if($('li').first().hasClass('active')) {
+      $('#previous').hide();
+    } else {
+      $('#previous').show();
+    }
+    if($('li').last().hasClass('active')) {
+      $('#next').hide();
+      $('#finish').show();
+    } else {
+      $('#next').show();
+
+    }
+    });
+
+
 });
